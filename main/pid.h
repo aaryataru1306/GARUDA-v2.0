38f6ff84e8e91ab31c4e3f268bfc4ef3f9d6ec0f
aaryataru1306 <aaryataru06@gmail.com>
2026-09-09 15:07:10 +0530

    Update pid...

--- main/pid.h
+++ main/pid.h
@@ -1,20 +1,18 @@
-#ifndef PID
-#define PID
+#ifndef PID_H
+#define PID_H
 
-typedef struct{
+typedef struct {
   float Kp;
   float Ki;
   float Kd;
-
   float integral;
   float previous_error;
-
   float output;
   float output_min;
   float output_max;
 } PID_struct;
 
-void PID_init(PID_struct *pid,float Kp,float Ki,float Kd,float output_min,float ouput_max);
-float PID_update(PID_struct *pid,float setpoint,float measurement,float dt);
+void PID_init(PID_struct *pid, float Kp, float Ki, float Kd, float output_min, float output_max);
+float PID_update(PID_struct *pid, float setpoint, float measurement, float dt);
 
 #endif
